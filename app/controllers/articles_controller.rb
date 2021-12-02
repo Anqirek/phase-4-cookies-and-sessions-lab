60f5b502f0e2225294afd109d56f5e8b7e4cbfff
Anthony Hill <hillganthony@outlook.com>
2021-12-02 13:27:36 -0500

    commit tree acbbfb621304ba86d8150c67fa45dba3aa87a400

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,15 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+
   def index
+
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
 
   def show
+    session[:pageviews] = 0
     article = Article.find(params[:id])
     render json: article
   end
@@ -17,4 +20,5 @@ class ArticlesController < ApplicationController
     render json: { error: "Article not found" }, status: :not_found
   end
 
+  # session[:pageviews_remaining] = 3
 end
